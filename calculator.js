aabd89045cf4deab48e955e4b7f57f7668596c1a
gluisa <76462853+gluisa@users.noreply.github.com>
2024-08-09 10:45:54 -0400

    Basic Functionality of adding subtraction, addition, division, and multiplication to the application

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function add (num1, num2) {
+    addResult = num1 + num2;
+    return addResult;
+}
+console.log(addResult);
+
+function subtract (num1, num2){
+    subtractResult = num1 - num2;
+    return subtractResult;
+}
+console.log(subtractResult);
+
+function multiply (num1, num2){
+    multiplyResult = num1 * num2;
+    return multiplyResult;
+}
+console.log(multiplyResult);
+
+function divide (num1, num2){
+    divideResult = num1 / num2;
+    return divideResult;
+}
+console.log(divideResult);
+
+
+
